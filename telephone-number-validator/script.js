a1ac44d2f12f360879738999969442316b982073
Mark Luo <mluo9987@gmail.com>
2025-07-11 11:18:20 -0400

    feat: Implemented phone number validation logic using regex

---
+++ telephone-number-validator/script.js
@@ -0,0 +1,24 @@
+const number = document.getElementById("user-input");
+const checkBtn = document.getElementById("check-btn");
+const clearBtn = document.getElementById("clear-btn");
+const resultsDiv = document.getElementById("results-div")
+
+checkBtn.addEventListener("click", ()=> {
+    const userInput = number.value.trim();
+    if (!userInput) {
+        alert("Please provide a phone number");
+    }
+
+    const validNumber = /^(1\s?)?(\(\d{3}\)|\d{3})([\s\-]?)\d{3}([\s\-]?)\d{4}$/;
+
+    if (validNumber.test(userInput)) {
+        resultsDiv.textContent = `Valid US number: ${userInput}`;
+    } else {
+        resultsDiv.textContent = `Invalid US number: ${userInput}`;
+    }
+
+});
+
+clearBtn.addEventListener("click", ()=> {
+    resultsDiv.textContent = "";
+});
